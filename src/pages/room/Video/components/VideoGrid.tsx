b9a8bd3ab026bbc1f7cfd92d6d7e4b5aa3424b42
Nilesh darji <97864081+Nilesh9106@users.noreply.github.com>
2024-05-27 13:59:56 +0530

    Nilesh/webrtc implementations (#14)

--- src/pages/room/Video/components/VideoGrid.tsx
+++ src/pages/room/Video/components/VideoGrid.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { User } from '@/types/auth';
 import useKlustrStore from '@/hooks/store';
 import { MicOff } from 'lucide-react';
@@ -10,12 +10,13 @@ type VideoProps = {
   user: User;
   isMuted?: boolean;
   rows: number;
+  cols: number;
   config: { audio: boolean; video: boolean };
 };
 
-const CustomVideo: React.FC<VideoProps> = ({ stream, user, isMuted, rows, config }) => {
+const CustomVideo: React.FC<VideoProps> = ({ stream, user, isMuted, rows, cols, config }) => {
   const videoRef = useRef<HTMLVideoElement>(null);
-  const { height } = useWindowDimensions();
+  const { height, width } = useWindowDimensions();
   useEffect(() => {
     const play = async () => {
       if (videoRef.current && stream) {
@@ -29,21 +30,20 @@ const CustomVideo: React.FC<VideoProps> = ({ stream, user, isMuted, rows, config
   return (
     <div
       style={{
-        height: `${(height - 216) / rows}px`,
-        width: '100%'
+        height: `${(height - 230) / rows - 6}px`,
+        width: Math.min(
+          ((height - 230) / rows) * (4 / 3) - 24,
+          (width - (width > 640 ? 80 : 0)) / cols - 24
+        )
       }}
-      className={`relative rounded-lg flex justify-center items-center h-full w-full ${
+      className={`relative rounded-lg flex justify-center items-center h-full ${
         isMuted || !config.audio ? '' : 'border-2 border-violet-600'
       }`}
     >
       <video
         ref={videoRef}
         muted={isMuted || !config.audio}
-        style={{
-          height: `${(height - 216) / rows}px`,
-          width: '100%'
-        }}
-        className={`${config.video ? '' : 'hidden'} rounded-lg w-full h-full `}
+        className={`${config.video ? '' : 'hidden'} rounded-lg h-full w-full object-cover`}
       />
       {!config.audio && (
         <div className="bg-black opacity-30 z-50 rounded-full p-1 absolute top-3 right-3 flex items-center space-x-2">
@@ -51,10 +51,7 @@ const CustomVideo: React.FC<VideoProps> = ({ stream, user, isMuted, rows, config
         </div>
       )}
       {!config.video && (
-        <div
-          style={{ height: (height - 216) / rows, width: '100%' }}
-          className="rounded-lg   flex items-center justify-center dark:bg-neutral-900 bg-neutral-200"
-        >
+        <div className="rounded-lg h-full w-full   flex items-center justify-center dark:bg-neutral-900 bg-neutral-200">
           <p className="font-bold">{user.username}</p>
         </div>
       )}
@@ -69,16 +66,49 @@ type Props = {
 };
 
 const VideoGrid = ({ peers, localStream, localConfig }: Props) => {
-  const totalVideos = Object.keys(peers).length + 1;
-  const columns = Math.ceil(Math.sqrt(totalVideos));
-  const rows = Math.ceil(totalVideos / columns);
+  const totalLength = Object.keys(peers).length + 1;
+  const [gridConfig, setGridConfig] = useState({ rows: 1, cols: 1 });
   const userInfo = useKlustrStore(state => state.userInfo);
+
+  useEffect(() => {
+    const updateGridConfig = () => {
+      const numVideos = totalLength;
+      let cols = Math.ceil(Math.sqrt(numVideos));
+      let rows = Math.ceil(numVideos / cols);
+      let width = window.innerWidth;
+      let height = window.innerHeight;
+      if (width > 640) {
+        height = height - 230;
+      } else {
+        height = height - 210;
+      }
+      if (width > 640) {
+        width = width - 80;
+      }
+
+      const aspectRatio = width / height;
+
+      while (cols / rows > aspectRatio) {
+        cols--;
+        rows = Math.ceil(numVideos / cols);
+      }
+
+      setGridConfig({ rows, cols });
+    };
+
+    updateGridConfig();
+    window.addEventListener('resize', updateGridConfig);
+
+    return () => window.removeEventListener('resize', updateGridConfig);
+  }, [totalLength]);
+
+  const { rows, cols } = gridConfig;
 
   return (
     <div
-      className="grid h-full gap-3 p-2"
+      className="grid h-full w-full gap-3 p-2 justify-items-center "
       style={{
-        gridTemplateColumns: `repeat(${columns}, 1fr)`,
+        gridTemplateColumns: `repeat(${cols}, 1fr)`,
         gridTemplateRows: `repeat(${rows}, 1fr)`
       }}
     >
@@ -87,6 +117,7 @@ const VideoGrid = ({ peers, localStream, localConfig }: Props) => {
           audio: localConfig.audio,
           video: localConfig.video
         }}
+        cols={cols}
         rows={rows}
         stream={localStream}
         user={userInfo!}
@@ -96,6 +127,7 @@ const VideoGrid = ({ peers, localStream, localConfig }: Props) => {
         return (
           <CustomVideo
             config={user.config}
+            cols={cols}
             rows={rows}
             key={user.user.id}
             stream={user.stream}
