129f2161f2b7d14b3f8ac03222ad6bc3f11f29c0
Nilesh darji <97864081+Nilesh9106@users.noreply.github.com>
2024-06-02 22:20:37 +0530

    Nilesh/deployment (#18)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,6 +6,7 @@ import { Link, Outlet, useNavigate } from 'react-router-dom';
 import { ModeToggle } from './mode-toggle';
 import { webRoutes } from '@/constants/routes';
 import { toast } from 'sonner';
+import LogoIcon from './LogoIcon';
 
 const routes = [
   {
@@ -42,7 +43,7 @@ export function Navbar() {
             to={webRoutes.home}
             className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
           >
-            <Package2 className="h-4 w-4 transition-all group-hover:scale-110" />
+            <LogoIcon color="white" className="size-6 transition-all group-hover:scale-110" />
             <span className="sr-only">Klustr</span>
           </Link>
           {routes.map((route, index) => (
