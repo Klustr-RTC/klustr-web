0cd247373aa14ced7e784f6a71c2b287bfce26aa
Nilesh darji <97864081+Nilesh9106@users.noreply.github.com>
2024-06-02 19:54:57 +0530

    Error Fixing (#17)

--- src/providers.tsx
+++ src/providers.tsx
@@ -1,6 +1,7 @@
 import { TooltipProvider } from '@radix-ui/react-tooltip';
 import { ThemeProvider } from './components/theme-provider';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import React from 'react';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
